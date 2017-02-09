6f92778898476525d6d787234bc97b253191c769
John Arcy Teodoro <john.arcy.r.teodoro@accenture.com>
2017-02-09 21:49:53 +0800

    edited to cater docker type extensions

--- bootstrap/Platform_Management/Load_Platform_Extension.groovy
+++ bootstrap/Platform_Management/Load_Platform_Extension.groovy
@@ -49,52 +49,69 @@ if [ -f ${WORKSPACE}/extension.metadata ]; then
     source ${WORKSPACE}/extension.metadata
 fi
 
-# Provision any EC2 instances in the AWS folder
-if [ -d ${WORKSPACE}/service/aws ]; then
-    
-    if [ -f ${WORKSPACE}/service/aws/service.template ] && [ -f ${WORKSPACE}/service/aws/cf-runner.yml ]; then
-        
-        echo "#######################################"
-        echo "Adding EC2 platform extension on AWS..."
+if [ $PLATFORM_EXTENSION_TYPE = "aws"  ]; then
+	# Provision any EC2 instances in the AWS folder
+	if [ -d ${WORKSPACE}/service/aws ]; then
+		
+		if [ -f ${WORKSPACE}/service/aws/service.template ] && [ -f ${WORKSPACE}/service/aws/cf-runner.yml ]; then
+			
+			echo "#######################################"
+			echo "Adding EC2 platform extension on AWS..."
 
-        cd service/aws/
-        ansible-playbook cf-runner.yml
-        if [[ $? -gt 0 ]]; then exit 1; fi
-        cd -
+			cd service/aws/
+			ansible-playbook cf-runner.yml
+			if [[ $? -gt 0 ]]; then exit 1; fi
+			cd -
 
-        if [ -f ${WORKSPACE}/service/aws/ec2-extension.conf ]; then
-                        
-            echo "#######################################"
-            echo "Adding EC2 instance to NGINX config using xip.io..."
-            
-            export SERVICE_NAME="EC2-Service-Extension-${BUILD_NUMBER}"
-            cp ${WORKSPACE}/service/aws/ec2-extension.conf ec2-extension.conf
-            NODE_IP=$(cat ${WORKSPACE}/service/aws/instance_ip.txt)
+			if [ -f ${WORKSPACE}/service/aws/ec2-extension.conf ]; then
+							
+				echo "#######################################"
+				echo "Adding EC2 instance to NGINX config using xip.io..."
+				
+				export SERVICE_NAME="EC2-Service-Extension-${BUILD_NUMBER}"
+				cp ${WORKSPACE}/service/aws/ec2-extension.conf ec2-extension.conf
+				NODE_IP=$(cat ${WORKSPACE}/service/aws/instance_ip.txt)
 
-            ## Add nginx configuration
-            sed -i "s/###EC2_SERVICE_NAME###/${SERVICE_NAME}/" ec2-extension.conf
-            sed -i "s/###EC2_HOST_IP###/${NODE_IP}/" ec2-extension.conf
-            docker cp ec2-extension.conf proxy:/etc/nginx/sites-enabled/${SERVICE_NAME}.conf
+				## Add nginx configuration
+				sed -i "s/###EC2_SERVICE_NAME###/${SERVICE_NAME}/" ec2-extension.conf
+				sed -i "s/###EC2_HOST_IP###/${NODE_IP}/" ec2-extension.conf
+				docker cp ec2-extension.conf proxy:/etc/nginx/sites-enabled/${SERVICE_NAME}.conf
 
-            ## Reload nginx
-            docker exec proxy /usr/sbin/nginx -s reload
+				## Reload nginx
+				docker exec proxy /usr/sbin/nginx -s reload
 
-            ## Don't stop jenkins to exit with error if nginx reload has failed.
-            if [[ $? -gt 0 ]]; then
-              echo "An error has been encountered while reloading nginx. There might be some upstreams that are not reachable."
-              echo "Please run 'docker exec proxy /usr/sbin/nginx -s reload' to debug nginx."      
-            fi
+				## Don't stop jenkins to exit with error if nginx reload has failed.
+				if [[ $? -gt 0 ]]; then
+				  echo "An error has been encountered while reloading nginx. There might be some upstreams that are not reachable."
+				  echo "Please run 'docker exec proxy /usr/sbin/nginx -s reload' to debug nginx."      
+				fi
 
-            echo "You can check that your EC2 instance has been succesfully proxied by accessing the following URL: ${SERVICE_NAME}.${PUBLIC_IP}.xip.io"
-        else
-            echo "INFO: /service/aws/ec2-extension.conf not found"
-        fi
-        
-    else
-        echo "INFO: /service/aws/service.template or /service/aws/cf-runner.yml not found."
-    fi
+				echo "You can check that your EC2 instance has been succesfully proxied by accessing the following URL: ${SERVICE_NAME}.${PUBLIC_IP}.xip.io"
+			else
+				echo "INFO: /service/aws/ec2-extension.conf not found"
+			fi
+			
+		else
+			echo "INFO: /service/aws/service.template or /service/aws/cf-runner.yml not found."
+		fi
+	fi
+elif [ $PLATFORM_EXTENSION_TYPE = "docker" ]; then
+
+	if [ -d ${WORKSPACE}/service/docker ]; then
+	
+		if [ -f ${WORKSPACE}/service/docker/docker-compose.yml ]; then
+		
+				echo "#######################################"
+				echo "Adding Docker-Compose configurations"
+				
+				cd service/docker
+				
+				docker-compose -f docker-compose.yml up -d
+		fi
+		
+	fi
+	
 fi
-
 ''')
     }
 }
