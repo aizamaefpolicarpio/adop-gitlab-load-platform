646987891192a7cce3990c2091b577a52bc8b5f5
John Bryan Sazon <john.bryan.j.sazon@accenture.com>
2017-09-27 20:32:11 +0800

    Add configure_sonarqube_installation.groovy

---
+++ sonar/groovy/configure_sonarqube_installation.groovy
@@ -0,0 +1,44 @@
+import hudson.model.*
+import jenkins.model.*
+import hudson.plugins.sonar.*
+import hudson.plugins.sonar.model.TriggersConfig
+import hudson.plugins.sonar.utils.SQServerVersions
+
+// Check if enabled
+def env = System.getenv()
+
+// Variables
+def sonarServerUrl = env['SONAR_SERVER_URL']
+def sonarToken = new File('/tmp/sonar_token_file').text.trim()
+def sonarInstallationName = "ADOP Sonar 5_3_or_HIGHER"
+
+if (!sonarServerUrl || !sonarToken) {
+    println "sonarServerUrl and sonarToken is empty, sonarqube installation will not proceed."
+    return
+}
+
+// Constants
+def instance = Jenkins.getInstance()
+
+// Sonar
+// Source: http://pghalliday.com/jenkins/groovy/sonar/chef/configuration/management/2014/09/21/some-useful-jenkins-groovy-scripts.html
+println "--> Configuring SonarQube"
+def SonarGlobalConfiguration sonarConf = instance.getDescriptor(SonarGlobalConfiguration.class)
+
+def sonar = new SonarInstallation(
+    sonarInstallationName,
+    sonarServerUrl,
+    SQServerVersions.SQ_5_3_OR_HIGHER,
+    sonarToken,
+    "",
+    "",
+    "",
+    "",
+    "",
+    new TriggersConfig(),
+    "",
+    "",
+    ""
+)
+
+sonarConf.setInstallations(sonar)
